8954ccbe28bc972d9b1eeffced102f1ffcf8e95f
zhaolifei <zhaolifei@ZBMAC-C02D42XTMD6M.local>
2020-09-14 11:28:07 +0800

    feat: 修改文案

--- src/components/goods-info/index.js
+++ src/components/goods-info/index.js
@@ -11,7 +11,7 @@ export default class GoodsInfo extends Component {
             {auctionBaseInfo && auctionBaseInfo.title}
           </div>
           <div styleName="goods-ext">
-            下单即享白条免洗，赠送限量版耳机，赠完为止！前5000名评价者可获得额外自拍杆！新品热卖中
+            该商品为新人专享商品，价格优惠，品质保障，快快抢购吧！
           </div>
           <div styleName="goods-promotion-icon">新人专享</div>
         </div>
