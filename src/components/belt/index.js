cb36889d629c13f5cabb32d591a0bc89b1e350f5
zhaolifei <zhaolifei@ZBMAC-C02D42XTMD6M.local>
2020-10-17 11:46:39 +0800

    feat: 参考价

--- src/components/belt/index.js
+++ src/components/belt/index.js
@@ -6,27 +6,36 @@ export default class Belt extends Component {
   render() {
     const { auctionBaseInfo } = this.props;
     const startPrice = auctionBaseInfo && auctionBaseInfo.startPrice;
+    const assessmentprice = auctionBaseInfo && auctionBaseInfo.assessmentprice;
     const price = formatPrice(startPrice);
     const priceArr = price.toString().split(".");
     const integer = priceArr[0];
     const decimal = priceArr[1];
     return (
       <div styleName="belt-container">
-        <div styleName="left">
-          <div styleName="price-txt">新人价</div>
-          <div styleName="price-flag">¥</div>
-          {startPrice > 0 && (
-            <div styleName="price">
-              <div>{integer}</div>
-              {decimal && decimal > 0 && (
-                <div styleName={`${integer > 0 ? "decimal" : ""}`}>
-                  .{decimal}
+        <div styleName="price-wrap">
+          <div styleName="price-container">
+            <div styleName="price-txt">新人价</div>
+            <div styleName="price-val">
+              <div styleName="price-flag">¥</div>
+              {startPrice > 0 && (
+                <div styleName="price">
+                  <div>{integer}</div>
+                  {decimal && decimal > 0 && (
+                    <div styleName="decimal">.{decimal}</div>
+                  )}
                 </div>
               )}
             </div>
+          </div>
+
+          {assessmentprice && assessmentprice > 0 && (
+            <div styleName="reference">
+              <div styleName="reference-price-txt">拍卖参考价</div>
+              <div styleName="reference-price">¥{assessmentprice}</div>
+            </div>
           )}
         </div>
-        <div styleName="right">新人专享，超值福利！</div>
       </div>
     );
   }
--- src/components/belt/index.stories.js
+++ src/components/belt/index.stories.js
@@ -9,7 +9,8 @@ storiesOf("单品页组件|Belt", module)
   .addDecorator(withKnobs)
   .add("商品腰带", () => {
     const auctionBaseInfo = {
-      startPrice: "100000"
+      startPrice: "100000.99",
+      assessmentprice: "999"
     };
     return <Belt auctionBaseInfo={auctionBaseInfo} />;
   });
