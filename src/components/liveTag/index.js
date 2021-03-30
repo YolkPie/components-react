8959b2f44706855c1f4ac9278e8e4bae19996f8c
zhangjie558 <zhangjie558@ZBMAC-C02F88LQQ05P.local>
2021-03-30 12:37:03 +0800

    feat: 增加liveTag

--- src/components/liveTag/index.js
+++ src/components/liveTag/index.js
@@ -2,17 +2,76 @@ import React, { Component } from "react";
 import "./index.scss";
 
 export default class LiveTag extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeInterVal: null,
+      timeCounter: null,
+      curLiveData: {}
+    };
+  }
+  _formatStartTime(leftTime) {
+    let seconds = leftTime / 1000;
+    let days = 0;
+    let hours = 0;
+    let min = 0;
+    let sec = 0;
+    if (seconds >= 86400) {
+      days = Math.floor(seconds / 86400);
+      seconds -= days * 86400;
+    }
+    if (seconds >= 3600) {
+      hours = Math.floor(seconds / 3600);
+      seconds -= hours * 3600;
+    }
+    if (seconds >= 60) {
+      min = Math.floor(seconds / 60);
+      seconds -= min * 60;
+    }
+    sec = Math.round(seconds);
+
+    hours = hours > 9 ? hours : `0${hours}`;
+    min = min > 9 ? min : `0${min}`;
+    sec = sec > 9 ? sec : `0${sec}`;
+    return `${days > 0 ? days + "天 " : ""}${hours}:${min}:${sec}`;
+  }
+
+  _startCounter(startTime) {
+    this.state.timeInterVal && clearInterval(this.state.timeInterVal);
+    let leftTime = startTime - Date.now();
+    let timeCounter = this._formatStartTime(leftTime);
+    let timeInterVal = setInterval(() => {
+      leftTime -= 1000;
+      if (leftTime <= 0) {
+        clearInterval(this.state.timeInterVal);
+        this.props.onCountEnd();
+        return;
+      }
+      timeCounter = this._formatStartTime(leftTime);
+      this.setState({ timeCounter });
+    }, 1000);
+    this.setState({ timeInterVal, timeCounter });
+  }
+  componentDidMount() {
+    const { liveList = [], liveValue } = this.props;
+    let curLiveData = liveList.find(item => item.value === liveValue) || {};
+    if (curLiveData.startTime > 0) {
+      this._startCounter(curLiveData.startTime);
+    }
+    this.setState({ curLiveData });
+  }
   render() {
-    const { liveData } = this.props;
-    return (
+    const { timeCounter, curLiveData } = this.state;
+    return curLiveData.code ? (
       <div styleName="tag-container">
-        <span styleName={"tag-txt-box tag-" + liveData.code}>
-          <span styleName="tag-txt">{liveData.label}</span>
+        <span styleName={"tag-txt-box tag-" + curLiveData.code}>
+          <span styleName="tag-txt">{curLiveData.label}</span>
         </span>
-        {liveData.subLabel && (
-          <span styleName="tag-sub">{liveData.subLabel}人</span>
+        {timeCounter && <span styleName="tag-sub">{timeCounter}</span>}
+        {curLiveData.viewNum && !timeCounter && (
+          <span styleName="tag-sub">{curLiveData.viewNum}</span>
         )}
       </div>
-    );
+    ) : null;
   }
 }
--- src/components/liveTag/index.stories.js
+++ src/components/liveTag/index.stories.js
@@ -9,10 +9,34 @@ storiesOf("UI组件|LiveTag", module)
   .addDecorator(withKnobs)
   .add("标签组件", () => {
     let liveList = [
-      { code: "preplay", label: "预告中", value: 1, subLabel: 159837 },
-      { code: "playing", label: "直播中", value: 2, subLabel: 20 },
-      { code: "replay", label: "回放", value: 3, subLabel: 40 },
-      { code: "will", label: "即将开播", value: 3, subLabel: "" }
+      {
+        code: "preplay",
+        label: "预告中11",
+        value: 2,
+        startTime: 1622977701000
+      },
+      { code: "playing", label: "直播中11", value: 4, viewNum: "20人" },
+      { code: "replay", label: "回放22", value: 6, viewNum: "40人" },
+      { code: "willPlay", label: "即将开播33", value: 8, viewNum: "" }
     ];
-    return liveList.map(item => <LiveTag liveData={item} />);
+    let liveValue = 2;
+    const _countEnd = () => {
+      liveValue = 4;
+      console.log("倒计时结束，5秒后刷新数据");
+      setTimeout(() => {
+        console.log("ajax refresh data");
+      }, 1000 * 5);
+    };
+    return (
+      <div>
+        <LiveTag
+          liveList={liveList}
+          liveValue={liveValue}
+          onCountEnd={() => _countEnd()}
+        />
+        <LiveTag liveList={liveList} liveValue={4} />
+        <LiveTag liveList={liveList} liveValue={6} />
+        <LiveTag liveList={liveList} liveValue={8} />
+      </div>
+    );
   });
